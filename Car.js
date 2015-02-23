8c7d85365a2981c8bbd6b6b2339eedad0380c6c8
Anuj Dhingra <adhingra@mobiquityinc.com>
2015-02-23 10:46:46 -0500

    Completed TDD Workshop

--- Car.js
+++ Car.js
@@ -5,8 +5,35 @@ var car = {
 	make	: "Citroen",
 	model	: "DS",
 	year	: 1975,
+	canFly  : "true",
+        altitude : 2000,
+	isConvertible : "true",
+	top : "up",
+	redButton : "inactive",
 	getName : function() {
 		return this.year + " " + this.make + " " + this.model;
+	},
+	putTopDown : function() {
+		if(this.altitude > 3000){
+		this.top = "up";
+		return "altitude too high";
+	}
+		else if(this.top=="up"){
+		this.top = "down"
+	}
+                return this.top;
+        },
+	putTopUp : function() {
+		if(this.top=="down"){
+                this.top = "up"
+                }
+                return this.top;
+        },
+	pressRedButton : function() {
+		this.redButton == "inactive" ? this.redButton = "active" : this.redButton = "inactive";  
+	},
+	canSwim : function(){
+		return this.redButton == "inactive" ? "false" : "true";
 	},
 	start : function() {
 		return this.year < 1985 ? "chugga chugga vroom vroom!" : "vroom vroom!";
--- CarTest.js
+++ CarTest.js
@@ -21,4 +21,46 @@ describe('Car', function() {
             assert.equal(testCar.start(), "vroom vroom!");
         })
     })
+    describe('when ensuring that a car can fly', function(){
+	it('should have wings', function(){
+	    should.exist(testCar.canFly);
+	})
+    })
+    describe('when a car can fly, it should have the notion of altitude', function(){
+        it('should have a notion of altitude', function(){
+	    if(testCar.canFly){
+            should.exist(testCar.altitude);
+	}
+        })
+    })
+    describe('when ensuring that a car can be a convertible', function(){
+        it('should be able to have the top go down', function(){
+            should.exist(testCar.isConvertible);
+        })
+    })
+    describe('when ensuring that if a car is convertible, it can make the roof go down', function(){
+        it('should return "up" or "down"', function(){
+            assert.equal(testCar.putTopDown(),"down");
+	    assert.equal(testCar.putTopUp(),"up");
+        })
+    })
+    describe('when a car has altitude > 3000 feet', function(){
+        it('should not be able to open top', function(){
+	    testCar.altitude = 4000;
+            if(testCar.altitude > 3000){
+	    assert.equal(testCar.putTopDown(),"altitude too high");
+	}
+        })
+    })
+    describe('when pressed the red button', function(){
+        it('should be able to move on water', function(){
+	    testCar.pressRedButton();
+            if(testCar.redButton == "active"){
+	    testCar.canSwim().should.equal("true");
+	}
+	    else{
+            testCar.canSwim().should.equal("false");
+	}	
+        })
+    })	
 });
